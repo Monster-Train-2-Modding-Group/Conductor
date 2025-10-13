2b78d522deef644dae821e292359ac332379b10d
Brandon <tricksterguy87@gmail.com>
2025-10-13 12:58:09 -0700

    Fix Ambiguous Method reference.

--- Conductor/Utilities.cs
+++ Conductor/Utilities.cs
@@ -8,8 +8,16 @@ namespace Conductor
         internal static FieldInfo TraitsSupportedInTooltips = AccessTools.Field(typeof(TooltipContainer), "TraitsSupportedInTooltips");
         internal static FieldInfo StatesSupportedInTooltips = AccessTools.Field(typeof(TooltipContainer), "StatesSupportedInTooltips");
 
+        internal static MethodInfo CreateAdditionalTooltips1 = AccessTools.Method(typeof(CardEffectBase), "CreateAdditionalTooltips",
+            [typeof(CardEffectState), typeof(TooltipContainer), typeof(SaveManager)]);
+        internal static MethodInfo CreateAdditionalTooltips2 = AccessTools.Method(typeof(CardEffectBase), "CreateAdditionalTooltips",
+            [typeof(CardEffectData), typeof(List<TooltipContent>), typeof(SaveManager), typeof(CardState)]);
+
+        internal static MethodInfo GetCardTooltipText = AccessTools.Method(typeof(CardTraitState), "GetCardTooltipText");
+
         /// <summary>
-        /// CardTraits have to be whitelisted to display a tooltip.
+        /// If a CardTraitState subclass defines GetCardTooltipText it does not display automatically when on a card.
+        /// CardTraits have to be whitelisted to display a tooltip. This function enables that.
         /// </summary>
         /// <param name="assembly">Optional assembly to pass in. If not specified the caller's assembly is assumed</param>
         public static void SetupTraitTooltips(Assembly? assembly)
@@ -21,13 +29,12 @@ namespace Conductor
                 // CardTraits that have a tooltip.
                 if (type.IsSubclassOf(typeof(CardTraitState)))
                 {
-                    bool needsATooltip = AccessTools.Method(type, "GetCardTooltipText").DeclaringType == type;
+                    bool needsATooltip = HasOverride(type, GetCardTooltipText);
                     if (needsATooltip)
                     {
                         cardTraitNames.Add(type.Name);
                         cardTraitNames.Add(type.AssemblyQualifiedName);
                     }
-
                 }
             }
             var traits = (HashSet<string>)TraitsSupportedInTooltips.GetValue(null);
@@ -35,7 +42,8 @@ namespace Conductor
         }
 
         /// <summary>
-        /// CardEffects have to be whitelisted to display a tooltip.
+        /// If a CardEffectBase subclass defines CreateAdditionalTooltips it won't display automatically when on a card.
+        /// CardEffects have to be whitelisted to display a tooltip. This function enables that.
         /// 
         /// </summary>
         /// <param name="assembly">Optional assembly to pass in. If not specified the caller's assembly is assumed</param>
@@ -48,7 +56,7 @@ namespace Conductor
                 // CardEffects that have a tooltip.
                 if (type.IsSubclassOf(typeof(CardEffectBase)))
                 {
-                    bool needsATooltip = AccessTools.Method(type, "CreateAdditionalTooltips").DeclaringType == type;
+                    bool needsATooltip = HasOverride(type, CreateAdditionalTooltips1) || HasOverride(type, CreateAdditionalTooltips2);
                     if (needsATooltip)
                     {
                         cardTraitNames.Add(type.AssemblyQualifiedName);
@@ -57,6 +65,20 @@ namespace Conductor
             }
             var states = (List<string>)StatesSupportedInTooltips.GetValue(null);
             states.AddRange(cardTraitNames);
+        }
+
+        internal static bool HasOverride(Type type, MethodInfo baseMethod)
+        {
+            if (baseMethod == null) 
+                return false;
+            var method = type.GetMethod(
+                baseMethod.Name,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
+                null,
+                baseMethod.GetParameters().Select(p => p.ParameterType).ToArray(),
+                null
+            );
+            return method != null && method.DeclaringType != baseMethod.DeclaringType;
         }
     }
 }
