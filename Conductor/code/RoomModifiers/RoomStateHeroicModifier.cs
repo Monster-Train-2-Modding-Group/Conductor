647454b1768ce704c1d58c4c43b2a5c90ed65438
Brandon <tricksterguy87@gmail.com>
2025-11-06 21:16:31 -0800

    Heroic implementation

---
+++ Conductor/code/RoomModifiers/RoomStateHeroicModifier.cs
@@ -0,0 +1,48 @@
+﻿using ShinyShoe.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Conductor.RoomModifiers
+{
+    /// <summary>
+    /// Not designed to be used directly. This drives the Heroic status effects implementation and is controlled by that status effect.
+    /// </summary>
+    public class RoomStateHeroicModifier : RoomStateModifierBase, IRoomStateModifier, IRoomStateDamageModifier, ILocalizationParamInt, ILocalizationParameterContext
+    {
+        public bool IsPreviewModeCopy { get; set; }
+        public int DamageModifier = 0;
+        public Team.Type TeamType = Team.Type.None;
+
+        public RoomStateHeroicModifier CopyForPreview()
+        {
+            RoomStateHeroicModifier roomStateHeroicModifier = new();
+            CopyBaseStateForPreview(roomStateHeroicModifier);
+            roomStateHeroicModifier.DamageModifier = DamageModifier;
+            roomStateHeroicModifier.TeamType = TeamType;
+            roomStateHeroicModifier.IsPreviewModeCopy = true;
+            return roomStateHeroicModifier;
+        }
+
+        public int GetModifiedAttackDamage(Damage.Type damageType, CharacterState attackerState, bool requestingForCharacterStats, ICoreGameManagers coreGameManagers)
+        {
+            if (attackerState.GetTeamType() == TeamType)
+            {
+                return DamageModifier;
+            }
+            return 0;
+        }
+
+        public int GetModifiedMagicPowerDamage(ICoreGameManagers coreGameManagers)
+        {
+            return 0;
+        }
+
+        public override bool GetShowTooltip()
+        {
+            return false;
+        }
+    }
+}
---
+++ Conductor/code/StatusEffects/StatusEffectHeroicState.cs
@@ -0,0 +1,58 @@
+﻿using Conductor.RoomModifiers;
+using ShinyShoe.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conductor.StatusEffects
+{
+    public class StatusEffectHeroicState : StatusEffectState
+    {
+        private RoomStateHeroicModifier? damageModifier;
+
+        protected override void CopyStateForPreviewInternal(StatusEffectState dest, List<IRoomStateModifier> characterRoomStateModifiers)
+        {
+            if (dest is StatusEffectHeroicState statusEffectHeroicState)
+            {
+                statusEffectHeroicState.damageModifier = damageModifier?.CopyForPreview();
+                int num = ((damageModifier != null) ? characterRoomStateModifiers.IndexOf(damageModifier) : (-1));
+                if (num >= 0)
+                {
+                    characterRoomStateModifiers[num] = statusEffectHeroicState.damageModifier!;
+                }
+            }
+        }
+
+        public override void OnStacksAdded(CharacterState character, int numStacksAdded, CharacterState.AddStatusEffectParams addStatusEffectParams, ICoreGameManagers coreGameManagers)
+        {
+            if (coreGameManagers.GetSaveManager().PreviewMode == IsPreviewModeCopy())
+            {
+                if (damageModifier == null)
+                {
+                    RoomModifierData setRoomModifierData = new RoomModifierData();
+                    damageModifier = new RoomStateHeroicModifier();
+                    damageModifier.Initialize(setRoomModifierData, coreGameManagers);
+                    damageModifier.IsPreviewModeCopy = IsPreviewModeCopy();
+                    damageModifier.TeamType = character.GetTeamType();
+                    character.AddNewCharacterRoomModifierState(damageModifier);
+                }
+                if (damageModifier != null)
+                {
+                    damageModifier.DamageModifier = GetAssociatedCharacter().GetStatusEffectStacks(GetStatusId());
+                    RoomManager roomManager = coreGameManagers.GetRoomManager();
+                    RoomState currentRoom = character.GetCurrentRoom();
+                    bool selected = roomManager.CurrentSelectedRoom == currentRoom.GetRoomIndex();
+                    currentRoom.UpdateRoomSelectedRoomStateModifiers(selected, coreGameManagers);
+                }
+            }
+        }
+
+        public override void OnStacksRemoved(CharacterState character, int numStacksRemoved, ICoreGameManagers coreGameManagers)
+        {
+            if (damageModifier != null)
+            {
+                damageModifier.DamageModifier = GetAssociatedCharacter().GetStatusEffectStacks(GetStatusId());
+            }
+        }
+    }
+}
