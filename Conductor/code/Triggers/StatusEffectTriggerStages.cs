b56c97935b4758fb8a3adad964ec482764aa4cc6
Brandon <tricksterguy87@gmail.com>
2025-11-06 22:45:38 -0800

    Add OnShift trigger stage

---
+++ Conductor/code/Triggers/StatusEffectTriggerStages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conductor.Triggers
+{
+    public static class StatusEffectTriggerStages
+    {
+        public static StatusEffectData.TriggerStage OnShift;
+    }
+}
---
+++ Conductor/patches/StatusEffectTriggerStagesPatches.cs
@@ -0,0 +1,26 @@
+﻿using Conductor.Triggers;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conductor.Patches
+{
+    [HarmonyPatch(typeof(HeroManager), nameof(HeroManager.ShiftCharacterTrigger))]
+    public class OnShiftStatusEffectTriggerStage
+    {
+        public static IEnumerator Postfix(IEnumerator __result, CharacterState charState, StatusEffectManager ___statusEffectManager)
+        {
+            while (__result.MoveNext())
+                yield return __result.Current;
+
+            StatusEffectState.InputTriggerParams inputTriggerParams = new()
+            {
+                associatedCharacter = charState
+            };
+            StatusEffectState.OutputTriggerParams outputTriggerParams = new();
+            yield return ___statusEffectManager.TriggerStatusEffectOnUnit(StatusEffectTriggerStages.OnShift, charState, inputTriggerParams, outputTriggerParams);
+        }
+    }
+}
